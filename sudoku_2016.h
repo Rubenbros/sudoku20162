d02ebbed50fb12d8e5532b367e57fdfd99d987db
David Abián <da@davidabian.com>
2016-10-19 21:20:42 +0200

    Update sudoku_2016.h

--- sudoku_2016.h
+++ sudoku_2016.h
@@ -1,19 +1,19 @@
-/* guarda para evitar inclusiones m˙ltiples ("include guard") */
+/* guarda para evitar inclusiones m√∫ltiples ("include guard") */
 #ifndef SUDOKU_H_2016
 #define SUDOKU_H_2016
 
 #include <inttypes.h>
 
-/* TamaÒos de la cuadricula */
-/* Se utilizan 16 columnas para facilitar la visualizaciÛn */
+/* Tama√±os de la cuadricula */
+/* Se utilizan 16 columnas para facilitar la visualizaci√≥n */
 enum {NUM_FILAS = 9, NUM_COLUMNAS = 16};
 
 /* Definiciones para valores muy utilizados */
 enum {FALSE = 0, TRUE = 1};
 
 typedef uint16_t CELDA;
-/* La informaciÛn de cada celda est· codificada en 16 bits
- * con el siguiente formato, empezando en el bit m·s significativo (MSB):
+/* La informaci√≥n de cada celda est√° codificada en 16 bits
+ * con el siguiente formato, empezando en el bit m√°s significativo (MSB):
  * 1 bit  PISTA
  * 1 bit  ERROR
  * 1 bit  no usado
@@ -40,10 +40,5 @@ sudoku_candidatos_propagar_arm(CELDA cuadricula[NUM_FILAS][NUM_COLUMNAS],
 int
 sudoku_candidatos_propagar_thumb(CELDA cuadricula[NUM_FILAS][NUM_COLUMNAS],
                                  uint8_t fila, uint8_t columna);
-
-void
-pruebasInitPropagar(CELDA cuadricula[NUM_FILAS][NUM_COLUMNAS],
-                    char init,
-                    char propagar);
 
 #endif /* SUDOKU_H_2016 */
